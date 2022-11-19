94b6d75e3e5104a0afe94f62de59493c4487d3f3
Luciana <luuluu135@gmail.com>
2022-11-18 21:05:49 -0300

    terminar el script

--- script.sh
+++ script.sh
@@ -1,27 +1,27 @@
 #!/bin/bash 
 
 #Guardo solo el numero de venta
-venta=$(curl -X GET 127.0.0.1:5000/usd |./jq '.venta')
+#venta=$(curl -X GET 127.0.0.1:5000/usd |./jq '.venta')
 
 #Guardo solo el numero de Compra
-compra=$(curl -X GET 127.0.0.1:5000/usd |./jq '.compra')
+#compra=$(curl -X GET 127.0.0.1:5000/usd |./jq '.compra')
 
 echo $(date +%Y-%m-%d_%H:%M) , $compra , $venta >> historico.txt
 
 
 while true
 do
-	curl -X GET 127.0.0.1:5000/usd |-jq '.compra' > compra.txt
+	curl -X GET 127.0.0.1:5000/usd 2>/dev/null | ./jq '.compra' > compra.txt
 	
 	comprar=$(tail -1 compra.txt)
 
-	curl -X GET 127.0.0.1:5000/usd |-jq '.venta' > venta.txt
+	curl -X GET 127.0.0.1:5000/usd 2>/dev/null | ./jq '.venta' > venta.txt
          
 	vender=$(tail -1 venta.txt)
 	
 	echo $(date  +%Y-%m-%d_%H:%M) , $compra , $venta > sync.txt
 
-	if [[$compra -ne $comprar || $venta -ne $vender]];then
+	if [[ $compra -ne $comprar || $venta -ne $vender ]];then
 
 		echo $(date +%Y-%m-%d_%H:%M) , $compra , $venta >> sync.txt
 
@@ -30,4 +30,4 @@ do
 	
 	fi	
 
-done &> /dev/null
+done
