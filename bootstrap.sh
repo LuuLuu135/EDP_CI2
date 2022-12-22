72f572cd81c2b5efef1fa5551e07ddfa125dc665
Luciana <luuluu135@gmail.com>
2022-12-22 18:01:10 -0300

    Solo queda probar el bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 install (){
-	SYNC_PATH=$(pwd)/script/sync.txt
+	SYNC_PATH=$(pwd)/script/syncc/sync.txt
 	sed "s|\#reemplazame|$SYNC_PATH|" servicio/service.template > servicio/servicio.service
 	sudo cp servicio/servicio.service /etc/systemd/system
 	sudo cp servicio/servicio.timer /etc/systemd/system
 	sudo systemcl daemon-reload
 	sudo systemctl start servicio.timer
 
-	sudo docker-compose up -d --build
+	sudo docker-compose up --build
 
 }
 
